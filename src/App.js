6eebf9e703a1d5d537fa2e1df4b0596ec40a7230
Aleksey <a.peresmekhin@action-media.ru>
2022-10-04 22:45:29 +0500

    Start implement login page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
+import classes from './components/Login/Login.module.css';
+import logo from './assets/logo.svg';
+import { FormInput } from './components/Shared/Forms/Inputs/FormInput';
+import { useEffect, useRef } from 'react';
+import { FormInputPassword } from './components/Shared/Forms/Inputs/FormInputPassword';
+import { FormButton } from './components/Shared/Forms/Buttons/FormButton';
+
 function App() {
   function onSubmit(event) {
     event.preventDefault();
   }
+  const textInput = useRef(null);
+
+  useEffect(() => {
+    textInput.current.focus();
+  }, []);
 
   return (
-    <section>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="login">
-          Enter your nickname
-          <input type="text" id="login" />
-        </label>
-        <label htmlFor="password">
-          Enter your password
-          <input type="password" id="password" />
-        </label>
-        <button type="submit">Sign in</button>
+    <section className={classes.container}>
+      <div className={classes.logo}>
+        <img src={logo} alt="Dog" className={classes.logoImg} />
+      </div>
+      <h1 className={classes.title}>Hello my little friend!</h1>
+      <form onSubmit={onSubmit} className={classes.form}>
+        <FormInput
+          ref={textInput}
+          id="login"
+          additionalClass={classes.formItem}
+          label="Enter your nickname"
+          type="text"
+        />
+        <FormInputPassword
+          id="password"
+          additionalClass={classes.formItem}
+          label="Enter your password"
+        />
+        <FormButton type="submit">Sign in</FormButton>
       </form>
     </section>
   );
